a0e462648cc86b4ce2222a066f630f58fa19988c
siddheshtodkar <stodkar53@gmail.com>
2025-07-09 11:18:33 +0530

    mock and spy

--- src/app/shared/services/users.service.spec.ts
+++ src/app/shared/services/users.service.spec.ts
@@ -2,13 +2,28 @@ import { TestBed } from '@angular/core/testing';
 
 import { UsersService } from './users.service';
 import { UserInterface } from '../types/user.interface';
+import { UtilsService } from './utils.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let utilsService: UtilsService // normal and spy
+
+  // Mock
+  // const utilsServiceMock = {
+  //   pluck: jest.fn()
+  // }
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      // providers: [UtilsService]
+      providers: [
+        UsersService,
+        UtilsService, // normal and spy
+        // { provide: UtilsService, useValue: utilsServiceMock } // Mock
+      ]
+    });
     service = TestBed.inject(UsersService);
+    utilsService = TestBed.inject(UtilsService)
   });
 
   it('should be created', () => {
@@ -28,6 +43,27 @@ describe('UsersService', () => {
       service.users = [{ id: "3", name: "john" }]
       service.removeUser("3")
       expect(service.users).toEqual([])
+    })
+  })
+
+  describe('getUsernames', ()=>{
+    it('should be usernames', ()=>{
+      // normal
+      service.users = [{id:'1', name:'Foo'}]
+      expect(service.getUsernames()).toEqual(['Foo'])
+
+      // spy
+      // jest.spyOn(utilsService, 'pluck')
+      // service.users = [{id:'1', name:'Foo'}]
+      // service.getUsernames()
+      // expect(utilsService.pluck).toHaveBeenCalledWith(
+      //   service.users,
+      //   'name'
+      // )
+
+      // Mock
+      // utilsServiceMock.pluck.mockReturnValue(["Foo"])
+      // expect(utilsServiceMock.pluck()).toEqual(['Foo'])
     })
   })
 });
--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -1,15 +1,20 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { UserInterface } from '../types/user.interface';
+import { UtilsService } from './utils.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
   users: UserInterface[] = []
+  utils = inject(UtilsService)
   addUser(user: UserInterface) {
     this.users = [...this.users, user]
   }
   removeUser(userId: string) {
     this.users = this.users.filter(user => user.id !== userId)
+  }
+  getUsernames(): string[] {
+    return this.utils.pluck(this.users, 'name')
   }
 }
